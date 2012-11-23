c046f7e03949d4a77ffe217b42459bd6c6a1b6d3
Josh Gargus <josh@schwa.ca>
2012-11-22 16:49:12 -0800

    Remove old placeholder files that were never used.

--- cross/Material.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Material.cpp
-//  Sketchy
-//
-//  Created by Joshua Gargus on 2/12/12.
-//  Copyright (c) 2012 Schwaftwarez. All rights reserved.
-//
-
-#include <iostream>
--- cross/Node.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Node.cpp
-//  Sketchy
-//
-//  Created by Joshua Gargus on 11/12/11.
-//  Copyright (c) 2011 Schwaftwarez. All rights reserved.
-//
-
-#include <iostream>
