37189f866e9d03d8239e1a35f88eb62679c8611b
Josh Gargus <josh@schwa.ca>
2013-12-14 13:59:05 -0800

    Indentation.

--- epoch1/lab/job/job01/time/time_util.h
+++ epoch1/lab/job/job01/time/time_util.h
@@ -18,29 +18,39 @@
 // schwa::job01::time =========================================================
 namespace schwa { namespace job01 { namespace time {
 
-	typedef std::chrono::high_resolution_clock clock;
-	typedef clock::time_point moment;
-	typedef std::chrono::milliseconds msecs;
+  typedef std::chrono::high_resolution_clock clock;
+  typedef clock::time_point moment;
+  typedef std::chrono::milliseconds msecs;
+  typedef std::chrono::microseconds usecs;
 
-	// This might seem scary, but the argument-type is what you
-	// get when you subtract one moment from another.  For example:
-	//    moment start_time = clock::now();
-	//    // do other stuff
-	//    msecs elapsed = duration(clock::now() - start_time)
-	//    cerr << "other stuff took " << elapsed::
-	template <class Rep, class Period>
-	inline msecs duration(const std::chrono::duration<Rep,Period>& dur) {
-		return std::chrono::duration_cast<msecs>(dur);
-	}
-
+  // This might seem scary, but the argument-type is what you
+  // get when you subtract one moment from another.  For example:
+  //    moment start_time = clock::now();
+  //    // do other stuff
+  //    msecs elapsed = duration(clock::now() - start_time)
+  //    cerr << "other stuff took " << elapsed::
+  template <class Rep, class Period>
+  inline msecs duration(const std::chrono::duration<Rep,Period>& dur) {
+    return std::chrono::duration_cast<msecs>(dur);
+  }
+  template <class Rep, class Period>
+  inline usecs duration_usecs(const std::chrono::duration<Rep,Period>& dur) {
+    return std::chrono::duration_cast<usecs>(dur);
+  }
 
 }  // schwa::job01::time ======================================================
    // schwa::job01 ============================================================
 
 // Print out time::msecs on ostream.
 inline std::ostream& operator<< (std::ostream &out, time::msecs& millis) {
-		out << millis.count() << "ms";
-		return out;
+  out << millis.count() << "ms";
+  return out;
+}
+
+// Print out time::usecs on ostream.
+inline std::ostream& operator<< (std::ostream &out, time::usecs& micros) {
+  out << micros.count() << "us";
+  return out;
 }
 
 }}  // schwa::job01 ===========================================================
