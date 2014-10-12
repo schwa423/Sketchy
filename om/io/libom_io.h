ab72ac5b3839ce33d4e4e0295da2d2929acee621
Josh Gargus <josh@schwa.ca>
2014-10-11 17:50:08 -0700

    .gitignore XCode build directories.
    Also, add a libom_io XCode proj, so that I can verify that I
    can do inter-project dependencies.

---
+++ om/io/libom_io.h
@@ -0,0 +1,13 @@
+//
+//  libom_io.h
+//  libom_io
+//
+//  Created by Josh Gargus on 10/11/14.
+//  Copyright (c) 2014 schwaftwarez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libom_io : NSObject
+
+@end
---
+++ om/io/libom_io.m
@@ -0,0 +1,13 @@
+//
+//  libom_io.m
+//  libom_io
+//
+//  Created by Josh Gargus on 10/11/14.
+//  Copyright (c) 2014 schwaftwarez. All rights reserved.
+//
+
+#import "libom_io.h"
+
+@implementation libom_io
+
+@end
